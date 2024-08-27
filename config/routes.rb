ba5c4fdcea217e3ed8f59546c81772e419ebbaff
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-27 12:21:25 +0000

    [Add]ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admin
+  devise_for :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'homes#top'
   
+  # admin内のルーティング
+  namespace :admin do
+    
+  end
   
+  
+  
+  # user内のルーティング
+  scope module: :user do
+    
+  end
   
   
 end
