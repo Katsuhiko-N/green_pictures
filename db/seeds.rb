05e9c65a44117afa2aa404ff5d3312539be1745e
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-09-07 12:31:32 +0000

    [Fix]seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,50 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "seed生成開始"
+puts "seed人物生成開始"
 taro = User.find_or_create_by!(email: "Taro@example.com") do |user|
   user.name = "太郎"
   user.nickname = "タロー"
+  user.body = "珍しい植物が見たい"
   user.password = "password"
-  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
 end
 
 jiro = User.find_or_create_by!(email: "Jiro@example.com") do |user|
   user.name = "二郎"
   user.nickname = "ジロー"
+  user.body = "キノコを見つけたい"
   user.password = "password"
-  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
 end
 
 hanako = User.find_or_create_by!(email: "Hanako@example.com") do |user|
   user.name = "花子"
   user.nickname = "ハナコ"
+  user.body = "花が好きです"
   user.password = "password"
-  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+  user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
-Post.find_or_create_by!(name: "庭に生えた草") do |post|
+
+puts "seed投稿生成開始"
+Post.find_or_create_by!(title: "庭に生えた草") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
   post.body = "名前がわからない草です。"
   post.user = taro
 end
 
-Post.find_or_create_by!(name: "珍しいキノコ") do |post|
+Post.find_or_create_by!(title: "珍しいキノコ") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
   post.body = "キノコ見つけた！"
   post.user = jiro
 end
 
-Post.find_or_create_by!(name: "綺麗な花") do |post|
+Post.find_or_create_by!(title: "綺麗な花") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
   post.body = '綺麗な花が咲いてた！'
   post.user = hanako
 end
+
+puts "seed生成完了"
