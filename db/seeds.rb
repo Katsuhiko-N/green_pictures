16dcb76bd95e2a9b25d23cfae31037e5bbf52923
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-09-16 13:04:41 +0000

    [Add]コメント一覧ページ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@
 puts "seed生成開始"
 
 puts "seed人物生成開始"
-
 taro = User.find_or_create_by!(email: "Taro@example.com") do |user|
   user.name = "太郎"
   user.nickname = "タロー"
@@ -33,6 +32,8 @@ hanako = User.find_or_create_by!(email: "Hanako@example.com") do |user|
   user.password = "password"
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
+puts "seed人物生成完了"
+
 
 
 puts "seed投稿生成開始"
@@ -53,11 +54,37 @@ Post.find_or_create_by!(title: "綺麗な花") do |post|
   post.body = '綺麗な花が咲いてた！'
   post.user = hanako
 end
+puts "seed投稿生成完了"
+
+
+
+puts "seedコメント生成開始"
+Comment.find_or_create_by!(id: "1") do |comment|
+  comment.post_id = "1"
+  comment.user_id = "3"
+  comment.body = "とっても大きくてワラビみたいな葉っぱですね"
+end
+
+Comment.find_or_create_by!(id: "2") do |comment|
+  comment.post_id = "2"
+  comment.user_id = "1"
+  comment.body = "冬虫夏草かな？見た感じはとっても小さそうだし、キノコっぽい"
+end
+
+Comment.find_or_create_by!(id: "3") do |comment|
+  comment.post_id = "3"
+  comment.user_id = "2"
+  comment.body = "隙間に見えるシロツメクサとはまた違った花だね"
+end
+
+puts "seedコメント生成完了"
+
+
 
 puts "管理者生成開始"
-
 admin = Admin.find_or_create_by!(email: ENV['AD_EMAIL'] ) do |admin|
   admin.password = ENV['AD_PASSWORD']
 end
+puts "管理者生成完了"
 
 puts "seed生成完了"
