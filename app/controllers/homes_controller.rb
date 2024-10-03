accdfd3a206611507df3d22fd92845446ebe513e
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-10-03 13:31:11 +0000

    デプロイ準備完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,9 +3,4 @@ class HomesController < ApplicationController
     def top
         
     end
-    
-    # テストページ
-    def v_test
-        @tags = Tag.all
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -89,9 +89,6 @@ Rails.application.routes.draw do
   # トップページ
   root to: 'homes#top'
   
-  # デバッグテストページ
-  get 'homes/v_test' => 'homes#v_test', as: "test"
-  
   # devise関係のルーティング
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
