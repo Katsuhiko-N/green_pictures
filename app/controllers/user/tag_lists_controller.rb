e1238772fb0a33d403d2292d1cef759d97d33cf2
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-10-03 16:41:35 +0000

    [Fix]消えたファイル復旧

---
+++ app/controllers/user/tag_lists_controller.rb
@@ -0,0 +1,27 @@
+class User::TagListsController < ApplicationController
+  # ログインしているか
+  before_action :authenticate_user!
+  # ゲストユーザーか？
+  before_action :ensure_guest_user
+  
+    def destroy
+        t_list = TagList.find_by(post_id: params[:post_id], tag_id: params[:tag_id])
+        t_list.destroy
+        
+        # 投稿と一個も結びつきのなくなったタグを削除
+        tags = Tag.left_outer_joins(:tag_lists).where(tag_lists: { id: nil })
+        tags.destroy_all
+        
+        redirect_to post_path(params[:post_id])
+    end
+    
+    # ゲストユーザーか識別# ゲストユーザーか識別
+    def ensure_guest_user
+        @user = User.find(current_user.id)
+        if @user.guest_user?
+            redirect_to posts_path, notice: "ゲストユーザーはこの操作を実行できません"
+        end
+    end
+    
+    
+end
