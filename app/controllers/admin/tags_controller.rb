336496e1f1c7f246ce31d8e0585d178ad8038e4e
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-10-20 13:50:48 +0000

    [Add]コメント・タグ一覧にページネーション追加

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -6,7 +6,7 @@ class Admin::TagsController < ApplicationController
   before_action :authenticate_admin!
     
     def index
-        @tags = Tag.all
+        @tags = Tag.page(params[:page])
     end
     
     def destroy
